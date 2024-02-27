d4c554c874909012cdd035d840fa1cc0f79c743c
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2024-02-27 14:51:04 -0800

    Agregar archivos de proyecto.

---
+++ CursoApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using CursoApp.Models;
+using PluginAPI;
+
+namespace CursoApp
+{
+    internal class Program
+    {
+        static bool running = true;
+        static string? apikey = string.Empty;
+
+        static void Main(string[] args)
+        {
+            ApiManager.Create("https://curso-dev-bmem.1.us-1.fl0.io");
+
+            onLogin();
+
+            while (running)
+            {
+
+            }
+        }
+
+        static async Task onLogin()
+        {
+            LoginModel cuenta = new LoginModel();
+            cuenta.email = "avalontm@curso.com";
+            cuenta.contrasena = "cinder";
+
+            string result = await ApiManager.Post(cuenta, "/api/login");
+
+            if(!result.GetValue<bool>("status", true))
+            {
+                Console.WriteLine($"Error: {result.GetValue<string>("message")}");
+                return;
+            }
+
+            apikey = result.GetValue<string>("api_key");
+
+            Console.WriteLine($"apikey: {apikey}");
+
+            if (!string.IsNullOrEmpty(apikey))
+            {
+                ApiManager.CreateKey("api-key", apikey);
+            }
+
+            result = await ApiManager.Get("/api/cursos");
+
+            Console.WriteLine($"result: {result}");
+        }
+    }
+}
