e5e02e4a37ab313c79d83dde61a6fd4ff28e6e83
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2024-02-29 12:13:46 -0800

    updated

--- CursoApp/Program.cs
+++ CursoApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CursoApp.Models;
+using Newtonsoft.Json.Linq;
 using PluginAPI;
 
 namespace CursoApp
@@ -12,7 +13,14 @@ namespace CursoApp
         {
             ApiManager.Create("https://curso-dev-bmem.1.us-1.fl0.io");
 
-            onLogin();
+            List<ConsoleMenuItem> opciones = new List<ConsoleMenuItem>();
+            opciones.Add(new ConsoleMenuItem() { Title = "Login", Action = onLogin });
+            opciones.Add(new ConsoleMenuItem() { Title = "Buscar Curso", Action = onFindCurso });
+            opciones.Add(new ConsoleMenuItem() { Title = "Ver Cursos", Action = onSeeCursos });
+            opciones.Add(new ConsoleMenuItem() { Title = "Completar Curso", Action = onCompletCurso });
+            opciones.Add(new ConsoleMenuItem() { Title = "Salir", Action = onExit });
+
+            ConsoleEx.Menu("Mini Curso", opciones);
 
             while (running)
             {
@@ -22,20 +30,24 @@ namespace CursoApp
 
         static async Task onLogin()
         {
-            LoginModel cuenta = new LoginModel();
+            // Crear un diccionario vacío
+            var cuenta = new Dictionary<string, string>();
 
             Console.WriteLine("Escribe tus credenciales.");
             Console.Write($"Email: ");
-            cuenta.email = Console.ReadLine();
+            cuenta.Add("email", Console.ReadLine());
 
             Console.Write($"Contraseña: ");
-            cuenta.contrasena = Console.ReadLine();
+            cuenta.Add("contrasena", ConsoleEx.ReadPassword());
 
+            await ConsoleEx.WaitStart("iniciando sesion");
             string result = await ApiManager.Post(cuenta, "/api/login");
+            await ConsoleEx.WaitEnd();
 
-            if(!result.GetValue<bool>("status", true))
+            if (!result.GetValue<bool>("status", true))
             {
                 Console.WriteLine($"Error: {result.GetValue<string>("message")}");
+                Continue();
                 return;
             }
 
@@ -49,9 +61,84 @@ namespace CursoApp
                 ApiManager.CreateKey("api-key", apikey);
             }
 
-            result = await ApiManager.Get("/api/cursos");
+            //esperamos que se precione cualquier tecla.
+            Continue();
+        }
 
-            Console.WriteLine($"result: {result}");
+        static async Task onFindCurso()
+        {
+
+            Console.WriteLine("Escribe el id del curso.");
+            Console.Write($"Id: ");
+            string curso_id = Console.ReadLine();
+
+
+            await ConsoleEx.WaitStart("obtiniendo informacion del curso");
+            string result = await ApiManager.Get($"/api/cursos/{curso_id}");
+            await ConsoleEx.WaitEnd();
+
+            List<JObject> cursos = result.GetValue<List<JObject>>("cursos");
+
+            ConsoleEx.CreateTable(cursos);
+
+            //esperamos que se precione cualquier tecla.
+            Continue();
+        }
+
+        private static async Task onSeeCursos()
+        {
+            await ConsoleEx.WaitStart("obteniendo mis cursos");
+            string result = await ApiManager.Get("/api/cursos");
+            await ConsoleEx.WaitEnd();
+
+            List<JObject> cursos = result.GetValue<List<JObject>>("cursos");
+
+            ConsoleEx.CreateTable(cursos);
+
+            //esperamos que se precione cualquier tecla.
+            Continue();
+        }
+
+        static async Task onCompletCurso()
+        {
+            var curso = new Dictionary<string, string>();
+
+            Console.WriteLine("Escribe el id del curso.");
+            Console.Write($"Id: ");
+            curso.Add("curso_id", Console.ReadLine());
+
+            Console.WriteLine("Escribe el token del curso.");
+            Console.Write($"Token: ");
+            curso.Add("token", Console.ReadLine());
+
+            await ConsoleEx.WaitStart("completando curso");
+            string result = await ApiManager.Post(curso, "/api/cursos/completar");
+            await ConsoleEx.WaitEnd();
+
+            if (!result.GetValue<bool>("status", true))
+            {
+                Console.WriteLine($"Error: {result.GetValue<string>("message")}");
+                Continue();
+                return;
+            }
+
+            Console.WriteLine($"{result.GetValue<string>("message")}");
+
+            //esperamos que se precione cualquier tecla.
+            Continue();
+        }
+
+        static async Task onExit()
+        {
+            running = false;
+            Environment.Exit(0);
+        }
+
+        static void Continue()
+        {
+            //esperamos que se precione cualquier tecla.
+            Console.WriteLine("Preciona una tecla para continuar...");
+            Console.ReadKey();
         }
     }
 }
